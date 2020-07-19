ffbba8fa9a91ab9eb9be1645d8ddf2bec3164792
Saikawind <834772398@qq.com>
2020-07-19 19:04:45 +0800

    Servlet入门

--- src/ServletDemo2.java
+++ src/ServletDemo2.java
@@ -1,9 +1,7 @@
 import javax.servlet.GenericServlet;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import java.io.IOException;
 
 /**
  * @Description: GenericServlet抽象类
---
+++ src/ServletDemo4.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description: 多个访问路径
+ * @Author: Wang Shiyu
+ * @Date: 2020/7/19/18:58
+ */
+@WebServlet({"/d4", "/demo4", "/Demo4"})
+public class ServletDemo4 extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("Demo4");
+    }
+}
